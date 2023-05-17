9e2337a2d351ae90cc873fb89b0d1b7c1306885f
Emre Dinç <99549830+EmreDinc10@users.noreply.github.com>
2023-05-17 16:03:02 +0300

    Update script.js

--- PlanApp/static/script.js
+++ PlanApp/static/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  $('#confirmButton').click(function() {
-      var userText = $('#inputArea').val();
-      $.ajax({
-          url: '/get_response',
-          data: { 'input': userText },
-          type: 'POST',
-          success: function(response) {
-              $('#displayArea').val(response.text);
-          },
-          error: function(error) {
-              console.log(error);
-          }
-      });
+    $('#confirmButton').click(function() {
+        var userText = $('#inputArea').val();
+        $.ajax({
+            url: '/get_response',
+            data: { 'input': userText },
+            type: 'POST',
+            success: function(response) {
+                $('#displayArea').html(response.table);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
   });
-});
-
+  
